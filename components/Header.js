0021a2905ae1a988c7d734405f4abad8261e89cc
Daniel Tsai <danieltsai.developer@gmail.com>
2021-08-26 18:44:10 -0700

    Update menu nav bar to use bootstrap 5

--- components/Header.js
+++ components/Header.js
@@ -1,102 +1,21 @@
 import React from "react";
-import { RichText, Link } from 'prismic-reactjs'
+import { RichText, Link } from 'prismic-reactjs';
+import { Nav, Navbar } from "react-bootstrap";
 
 const Header = ({ menu = [] }) => (
-    <header className="site-header">
-      <a href="/" className="logo">
-        {RichText.asText(menu.data.title)}
-      </a>
-      <Links menuLinks={menu.data.menu_links} />
-      <style jsx>{`
-        .site-header {
-          height: 30px;
-          padding: 20px 0;
-          color: #484d52;
-          font-weight: 700;
-        }
-        .site-header a {
-          color: #484d52;
-          font-weight: 700;
-        }
-        .site-header a:hover {
-          color: #72767B;
-        }
-        .site-header .logo {
-          display: inline-block;
-          font-size: 22px;
-          font-weight: 900;
-        }
-        @media (max-width: 1060px) {
-          .site-header {
-            padding-left: 20px;
-            padding-right: 20px;
-          }
-        }
-        @media (max-width: 767px) {
-          .site-header {
-            height: auto;
-          }
-          .site-header {
-            position: absolute;
-            left: 0;
-            right: 0;
-          }
-          .site-header .logo {
-            display: block;
-            text-align: center;
-          }
-        `}</style>
-    </header>
+  <Navbar sticky="top" bg="dark" variant="dark" expand="md">
+    <Navbar.Brand href="/">{RichText.asText(menu.data.title)}</Navbar.Brand>
+    <Navbar.Toggle aria-controls="main-navbar-nav"/>
+    <Navbar.Collapse id="main-navbar-nav">
+      <Nav className="me-auto">
+        {menu.data.menu_links.map((menu_link) => (
+          <Nav.Link key={menu_link.link} href={Link.url(menu_link.link)}>
+            {RichText.asText(menu_link.label)}
+          </Nav.Link>
+        ))}
+      </Nav>
+    </Navbar.Collapse>
+  </Navbar>
 );
-
-const Links = ({menuLinks}) => {
-  if (menuLinks) {
-    return (
-      <nav>
-        <ul>
-          {menuLinks.map((menuLink, index) => (
-            <li key={`menulink-${index}`}>
-              <a href={Link.url(menuLink.link)}>
-                {RichText.asText(menuLink.label)}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <style jsx>{`
-          nav {
-            float: right;
-          }
-          nav ul {
-            margin: 0;
-            padding-left: 0;
-          }
-          nav li {
-            display: inline-block;
-            margin-left: 40px;
-          }
-          nav li a {
-            color: #484d52;
-            font-weight: 700;
-          }
-          nav li a:hover {
-            color: #72767B;
-          }
-          @media (max-width: 767px) {
-            nav {
-              float: none;
-              text-align: center;
-            }
-            nav li {
-              display: inline-block;
-              margin-left: 10px;
-              margin-right: 10px;
-            }
-          }
-          `}</style>
-      </nav>
-    )
-  }
-  return null
-}
 
 export default Header;
