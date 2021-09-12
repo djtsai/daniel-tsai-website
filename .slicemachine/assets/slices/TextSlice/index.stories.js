fd13e16ce7b12a7e74925c22e6124639a9108bfc
Daniel Tsai <danieltsai.developer@gmail.com>
2021-09-12 11:06:56 -0700

    Add TimelineSlice

--- .slicemachine/assets/slices/TextSlice/index.stories.js
+++ .slicemachine/assets/slices/TextSlice/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"text_slice","items":[],"primary":{"text":[{"type":"paragraph","text":"Ex tempor do dolor ad labore qui ex veniam aliqua commodo ipsum deserunt irure dolor. Occaecat reprehenderit consectetur excepteur ex quis.","spans":[]}]},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"text_slice","items":[],"primary":{"text":[{"type":"paragraph","text":"Ad ullamco nisi amet laboris tempor minim irure in occaecat ipsum sint commodo reprehenderit amet. Eu non veniam quis.","spans":[]}]},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
---
+++ .slicemachine/assets/slices/TimelineSlice/index.stories.js
@@ -0,0 +1,9 @@
+import MyComponent from '../../../../slices/TimelineSlice';
+
+export default {
+  title: 'slices/TimelineSlice'
+}
+
+
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"timeline_slice","items":[{"dateString":"unleash integrated bandwidth","title":"optimize ubiquitous partnerships","subtitle":"mesh viral platforms","description":"integrate ubiquitous functionalities"},{"dateString":"recontextualize virtual content","title":"streamline virtual niches","subtitle":"grow clicks-and-mortar e-tailers","description":"synthesize wireless mindshare"},{"dateString":"brand 24/7 niches","title":"orchestrate visionary experiences","subtitle":"seize next-generation markets","description":"disintermediate proactive solutions"},{"dateString":"engineer cross-media portals","title":"engage efficient content","subtitle":"strategize strategic markets","description":"empower cross-media synergies"},{"dateString":"aggregate dot-com models","title":"scale vertical eyeballs","subtitle":"benchmark web-enabled platforms","description":"optimize efficient e-tailers"}],"primary":{},"id":"_DefaultSlice"}} />
+_DefaultSlice.storyName = 'Default slice'
