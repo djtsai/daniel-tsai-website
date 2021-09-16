346be765da1f1a85c47b8f2b356383ce013df2f6
Daniel Tsai <danieltsai.developer@gmail.com>
2021-09-15 23:47:01 -0700

    Change links to content relationship

--- .slicemachine/assets/slices/TextSlice/index.stories.js
+++ .slicemachine/assets/slices/TextSlice/index.stories.js
@@ -5,5 +5,5 @@ export default {
 }
 
 
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"text_slice","items":[],"primary":{"text":[{"type":"paragraph","text":"Ad ullamco nisi amet laboris tempor minim irure in occaecat ipsum sint commodo reprehenderit amet. Eu non veniam quis.","spans":[]}]},"id":"_DefaultSlice"}} />
+export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"text_slice","items":[],"primary":{"text":[{"type":"paragraph","text":"Ullamco do Lorem et nulla do ut aliquip irure quis enim consequat laboris. Incididunt velit culpa aliqua exercitation dolor proident sit exercitation cillum id tempor. Eiusmod adipisicing amet anim.","spans":[]}]},"id":"_DefaultSlice"}} />
 _DefaultSlice.storyName = 'Default slice'
