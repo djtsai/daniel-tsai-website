9d6f7e5e6a28b9650547cd9f5c7fb859182829ba
Daniel Tsai <danieltsai.developer@gmail.com>
2023-06-23 12:17:53 -0700

    Set up preview routes

---
+++ pages/api/preview.ts
@@ -0,0 +1,9 @@
+import { setPreviewData, redirectToPreviewURL } from '@prismicio/next'
+import type { NextApiRequestLike, NextApiResponseLike } from '@prismicio/next/src/types'
+import { createClient } from '@/prismicio'
+
+export default async (req: NextApiRequestLike, res: NextApiResponseLike) => {
+  const client = createClient({ req })
+  await setPreviewData({ req, res })
+  await redirectToPreviewURL({ req, res, client })
+}
