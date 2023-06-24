f6cd27311dac5b8dcb90b4b6c65254dfabdb3d58
Daniel Tsai <danieltsai.developer@gmail.com>
2023-06-24 00:48:49 -0700

    Delete old slice machine configs

--- .slicemachine/assets/slices/TextSlice/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/TextSlice';
-
-export default {
-  title: 'slices/TextSlice'
-}
-
-
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"text_slice","items":[],"primary":{"text":[{"type":"paragraph","text":"Duis commodo consequat nostrud officia sint adipisicing esse. Nostrud commodo aliqua commodo nulla id. Labore enim cillum ipsum veniam cillum reprehenderit id Lorem anim non veniam.","spans":[]}]},"id":"_DefaultSlice"}} />
-_DefaultSlice.storyName = 'Default slice'
